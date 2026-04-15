645b8e489064f8e2111357f8de5728c8074d4eb2
Mati <90572281+matiEU07@users.noreply.github.com>
2026-04-15 14:09:19 +0200

    removed home

--- BigBrotherSRV/App.xaml.cs
+++ BigBrotherSRV/App.xaml.cs
@@ -10,7 +10,7 @@ namespace BigBrotherSRV
 
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new MainPage();
 
             StartServer();
             StartUDP();
